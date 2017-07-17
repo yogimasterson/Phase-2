31e4ebcf9e521f8f00ad4bdc4bcb4a70bdead9d6
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-07-17 13:41:17 -0700

    almost finished with movie search api

--- movie-search-api/index.js
+++ movie-search-api/index.js
@@ -3,15 +3,16 @@ const app = express()
 const cheerio = require('cheerio')
 const rp = require('request-promise')
 
-app.get('/', (req, res) => {
-	res.send('Hello World')
+app.get('/api/imdb/search/:query', (req, res) => {
+	const JQuery = req.params.query
+	res.send(queryIMDB(JQuery, null))
 })
 
 app.listen(3000, () => {
 	console.log('Ex app listening on port 3000!')
 })
 
-function queryIMDB(search, cb) {
+const queryIMDB = (search, cb) => {
 
 	const options = {
 		uri: 'http://www.imdb.com/find',
@@ -26,22 +27,30 @@ function queryIMDB(search, cb) {
 	}
 
 	return rp(options)
-		.then(function ($) {
+		.then(function ($) {\
 			const movieNames = $('.findSection')
 				.first()
-				.find('.result_text')
+				.find('.result_text a')
+				.not('small a')
 				.map((i, elem) => $(elem).text())
 				.toArray()
-			console.log(movieNames.join('\n'))
-			return movieNames.join('\n')
+			console.log(movieNames)
+
+			const movieYears = $('.findSection') 
+				.first()
+				.find('.result_text')
+				.not('small a')
+				.map((i, elem) => $(elem).text().match(/\(({.*})\)/))
+				.toArray()
+			console.log(movieYears)
 		})
 		.catch(function (err) {
 			console.log('You have an error: ' + err)
 			throw err
 		})
-}	
+}
 
-function run() {
+const run = () => {
 	const search = process.argv[2]
 
 	queryIMDB(search)
