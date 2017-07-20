ce680792df7df3f6685a49d5f181190fe213586a
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-07-20 14:45:32 -0700

    started work on movie search engine

---
+++ movie-search-engine/api.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio')
+const rp = require('request-promise')
+
+function queryIMDB(search) {
+
+	const options = {
+		uri: 'http://www.imdb.com/find',
+		qs: {
+			ref_: 'nv_sr_fn',
+			q: search,
+			s: 'all',
+		},
+		transform: function (body) {
+			return cheerio.load(body)
+		}
+	}
+
+	return rp(options)
+		.then(function ($) {
+			const movieNames = $('.findSection')
+				.first()
+				.find('.result_text')
+				.map((i, elem) => $(elem).text())
+				.toArray()
+			console.log(movieNames.join('\n'))
+			return movieNames.join('\n')
+		})
+		.catch(function (err) {
+			console.log('You have an error: ' + err)
+			throw err
+		})
+}
+
+module.exports = queryIMDB
--- movie-search-engine/app.js
+++ movie-search-engine/app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const promise = require('bluebird')
 const path = require('path')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+
+const queryIMDB = require('./api.js')
 
 const options = {
 	promiseLib: promise
@@ -12,14 +17,56 @@ const connectionString = ('postgres://localhost:5432/movies')
 const db = pgp(connectionString)
 
 const app = express()
-app.set('view engine', 'ejs')
 
 //middleware
-app.use(bodyParser.urlencoded({ extends: true }))
+app.use(bodyParser.urlencoded({
+	extends: true
+}))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(session({ secret: 'keyboard cat' }))
+
+app.set('view engine', 'ejs')
+
+let err = ''
+app.locals.err = err
 
 app.get('/', (req, res) => {
 	res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+	res.render('login.ejs')
+})
+
+app.post('/login', (req, res) => {
+	
+})
+
+app.get('/register', (req, res) => {
+	res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+	db.none('INSERT INTO users(name, email, password) VALUES($1, $2, $3)', [req.body.name, req.body.email, req.body.password])
+		.then( () => {
+			res.redirect('/search')
+		})
+		.catch((error) => {
+			err = 'Email is already registered, try another.'
+			if (err) {
+				res.render('login.ejs', {err: err})
+			}
+		})
+})
+
+app.get('/search', (req, res) => {
+	res.render('search.ejs')
+})
+
+app.post('/search', (req, res) => {
+	queryIMDB(req.body.search)
 })
 
 const port = 3000
